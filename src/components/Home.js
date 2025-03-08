3ebc22967a49aae8adba1a7c3474f64432495ddb
Kunal <kunaljsr11@gmail.com>
2025-03-08 20:16:53 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Notes from "./Notes";
+
+// import Noteitem from "./Noteitem";
+
+export default function notes() {
+  return (
+    <div>
+
+      <Notes/>
+      {/* <div className="row">
+        <Noteitem note={note}/>
+      </div> */}
+    </div>
+  );
+}
